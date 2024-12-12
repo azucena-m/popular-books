57b203b51fbff414cfdfd2001a8f363cf3451c78
Azucena Marroquin <atrevizo4@cnm.edu>
2024-12-12 15:32:35 -0700

    web scraping, installed requests

--- main.py
+++ main.py
@@ -4,9 +4,9 @@
 #allow users to select a genre 
 #generate the top 10 based on genre
 #save list to excel sheet
-import pandas as pd
-from bs4 import BeautifulSoup
-from selenium import webdriver
+# import pandas as pd
+# from bs4 import BeautifulSoup
+# from selenium import webdriver
 # book_genre = ('romance', 'fantasy', 'young adult', 'sicence fiction', 'self-help', 'romantasy', 'historical fantasy', 'mystery', 'thriller')
 # print('These are the current genre options: \n' )
 # for genre in book_genre:
@@ -14,16 +14,18 @@ from selenium import webdriver
 
 # coice = input('What genre are you interested in? ').lower()
 
-# driver = webdriver.Chrome()
-# driver.get('https://sandbox.oxylabs.io/products')
-# url = 'https://www.oxylabs.io/blog'
-# results = []
-# content = driver.page_source
-# soup = BeautifulSoup(content, 'html.parser')
-# for element in soup.find_all(attrs={'class': 'product-card'}):
-#     name = element.find('h4')
-#     if name not in results:
-#         results.append(name.text)
+driver = webdriver.Chrome()
+driver.get('https://www.barnesandnoble.com/b/books/_/N-1fZ29Z8q8')
+
+results = []
+content = driver.page_source
+soup = BeautifulSoup(content, 'html.parser')
+for element in soup.find_all(attrs={'class': 'product-shelf-title'}):
+    name = element.find('a')
+    if name not in results:
+        results.append(name.text)
+for x in results:
+    print(x)
 # df = pd.DataFrame({'Names': results})
 # df.to_excel('names.xlsx', index=False)
 
